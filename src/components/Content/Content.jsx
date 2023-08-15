5490a7d87ebeba167a47af3187cfb274613285fb
masudmolla6 <masudmolla2937@gmail.com>
2023-08-15 20:56:46 +0600

    set title

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -7,13 +7,16 @@ const Content = () => {
     const topics = allTopics.data;
     console.log(topics);
     return (
-        <div className='grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-2 mx-20 mt-4'>
+        <div>
+            <h1 className='text-5xl text-center text-blue-400 italic'>Check YourSelf</h1>
+            <div className='grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-2 mx-20 mt-4'>
             {
                 topics.map(subject => <Subject
                     key={subject.id}
                     subject={subject}
                 ></Subject>)
             }
+            </div>
         </div>
     );
 };
